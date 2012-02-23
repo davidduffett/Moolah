ea1bbb25f13aeef900e3a93f661a67938a327348
David Duffett <duffett.david@gmail.com>
2012-02-23 17:06:04 +0000

    Build scripts and tools added

---
+++ Moolah/build.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+if '%1' == '/?' goto usage
+if '%1' == '-?' goto usage
+if '%1' == '?' goto usage
+if '%1' == '/help' goto usage
+if '%1' == '--help' goto usage
+if '%1' == '-help' goto usage
+
+SET NANT="..\tools\nant\nant.exe"
+SET BUILD_FILE="%~dp0main.build"
+
+%NANT% /f:%BUILD_FILE% %*
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
+goto finish
+
+:usage
+echo.
+echo Usage: build.bat
+echo.
+goto finish
+
+:errors
+EXIT /B %ERRORLEVEL%
+
+:finish
