b947af4e16ef239f2a91a2b259ab235680f11494
Alex Friedman <a.friedman07@gmail.com>
2012-11-15 00:07:07 -0500

    Added Refunds for PayPal

---
+++ Moolah/Moolah/PayPal/PaypalRefundResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Specialized;
+
+namespace Moolah.PayPal
+{
+    public class PaypalRefundResponse
+    {
+        public PaymentStatus Status { get; set; }
+        public string TransactionId { get; set; }
+    }
+}
--- Moolah/Moolah/PaymentGateways.cs
+++ Moolah/Moolah/PaymentGateways.cs
@@ -95,6 +95,17 @@ namespace Moolah
         /// <param name="payPalToken">The PayPal token returned in the initial <see cref="SetExpressCheckout"/> call.</param>
         /// <param name="payPalPayerId">The PayPal PayerID returned in the <see cref="GetExpressCheckoutDetails"/> call.</param>        
         IPaymentResponse DoExpressCheckoutPayment(OrderDetails orderDetails, string payPalToken, string payPalPayerId);
+
+        /// <summary>
+        /// Performs a refund for the specified transaction.
+        /// </summary>
+        /// <param name="transactionId">The transaction Id received from the DoExpressCheckoutPayment call.</param>
+        /// <param name="refundType">Type of refund. Default is full.</param>
+        /// <param name="amount">The amount to refund. This parameter is only needed for partial refunds.</param>
+        /// <param name="description">A note about the refund. Only needed for partial refunds.</param>
+        /// <param name="currencyCodeType">The Currency Code for the refund. Only needed for partial refunds.</param>
+        /// <returns>The status of the refund.</returns>
+        PaypalRefundResponse RefundTransaction(string transactionId, RefundType refundType = RefundType.Full, decimal amount = 0, string description = null, CurrencyCodeType currencyCodeType = CurrencyCodeType.USD);
     }
 
     public interface IGoogleCheckout
