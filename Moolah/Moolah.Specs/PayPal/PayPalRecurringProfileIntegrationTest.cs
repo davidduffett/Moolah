dad93e0951b4738beb3b9b63c8d4599832ae4777
Enrique <niofis@gmail.com>
2013-04-15 09:20:21 -0700

    Added integration test for paypal recurring profiles.

---
+++ Moolah/Moolah.Specs/PayPal/PayPalRecurringProfileIntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using Machine.Fakes;
+using Machine.Specifications;
+using Machine.Specifications.Model;
+using Moolah.PayPal;
+
+namespace Moolah.Specs.PayPal
+{
+
+    [Subject(typeof(PayPalExpressCheckout))]
+    public class When_registering_a_recurring_profile : PayPalExpressCheckoutContext
+    {
+        It should_return_the_correct_result = () =>
+            Result.ShouldEqual(ExpectedResult);
+
+        Establish context = () =>
+        {
+            ExpectedResult = new PayPalRecurringProfileResponse();
+            RequestBuilder.WhenToldTo(x => x.CreateRecurringPaymentsProfile(Profile, Token))
+                .Return(HttpUtility.ParseQueryString(Request));
+            ResponseParser.WhenToldTo(x => x.CreateRecurringProfile(Param<NameValueCollection>.Matches(r => r.ToString() == Response)))
+                .Return(ExpectedResult);
+        };
+
+        private Because of = () =>
+                             Result = SUT.CreateRecurringPaymentsProfile(Profile, Token);
+
+        static IPaymentResponse Result;
+        static PayPalRecurringProfileResponse ExpectedResult;
+        const string Token = "tokenValue";
+        static RecurringProfile Profile = new RecurringProfile();
+        const string PayerId = "payerId";
+    }
+}
