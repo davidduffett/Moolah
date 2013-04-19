d3d5584e0017446d42ec3e7597277a2131bc5249
Enrique <niofis@gmail.com>
2013-04-18 17:01:07 -0700

    New integration tets for recurring profiles.

--- Moolah/Moolah.Specs/PayPal/PayPalRecurringProfileIntegrationTest.cs
+++ Moolah/Moolah.Specs/PayPal/PayPalRecurringProfileIntegrationTest.cs
@@ -8,29 +8,128 @@ using Moolah.PayPal;
 
 namespace Moolah.Specs.PayPal
 {
-
-    [Subject(typeof(PayPalExpressCheckout))]
-    public class When_registering_a_recurring_profile : PayPalExpressCheckoutContext
+    [Behaviors]
+    public class SuccesfulRecurringProfileCreationBehavior
     {
-        It should_return_the_correct_result = () =>
-            Result.ShouldEqual(ExpectedResult);
+        It should_get_a_successful_response = () =>
+            Response.Status.ShouldEqual(PaymentStatus.Pending);
 
-        Establish context = () =>
+        It should_not_specify_a_failure_message = () =>
+            Response.FailureMessage.ShouldBeNull();
+
+        It should_get_a_checkout_token = () =>
+            Response.PayPalToken.ShouldNotBeEmpty();
+
+        It should_provide_a_redirect_url = () =>
         {
-            ExpectedResult = new PayPalRecurringProfileResponse();
-            RequestBuilder.WhenToldTo(x => x.CreateRecurringPaymentsProfile(Profile, Token))
-                .Return(HttpUtility.ParseQueryString(Request));
-            ResponseParser.WhenToldTo(x => x.CreateRecurringProfile(Param<NameValueCollection>.Matches(r => r.ToString() == Response)))
-                .Return(ExpectedResult);
+            Response.RedirectUrl.ShouldNotBeEmpty();
+            System.Diagnostics.Debug.WriteLine("Redirect to: " + Response.RedirectUrl);
         };
 
-        private Because of = () =>
-                             Result = SUT.CreateRecurringPaymentsProfile(Profile, Token);
+        protected static PayPalExpressCheckoutToken Response;
+    }
 
-        static IPaymentResponse Result;
-        static PayPalRecurringProfileResponse ExpectedResult;
-        const string Token = "tokenValue";
-        static RecurringProfile Profile = new RecurringProfile();
-        const string PayerId = "payerId";
+    public class When_starting_express_checkout_with_single_subsription : ExpressCheckoutContext
+    {
+        Behaves_like<SuccessfulExpressCheckoutBehavior> a_successful_express_checkout;
+        Establish context = () =>
+                OrderDetails = new OrderDetails
+                                   {
+                                       OrderDescription = "Some order",
+                                       Items = new[]
+                                                   {
+                                                       new OrderDetailsItem
+                                                           {
+                                                               Description = "Subscription",
+                                                               Name = "SBC",
+                                                               Number = 1,
+                                                               Quantity = 1,
+                                                               UnitPrice = 0.0m, 
+                                                               ItemUrl = "http://localhost/subscription?1",
+                                                               IsRecurrentPayment = true
+                                                           }
+                                                   },
+                                       ShippingTotal = 0m,
+                                       OrderTotal = 0m
+                                   };
+    }
+
+    public class When_starting_express_checkout_with_items_and_subscription : ExpressCheckoutContext
+    {
+        Behaves_like<SuccessfulExpressCheckoutBehavior> a_successful_express_checkout;
+        Establish context = () =>
+                OrderDetails = new OrderDetails
+                {
+                    OrderDescription = "Some order",
+                    Items = new[]
+                                                   {
+                                                       new OrderDetailsItem
+                                                           {
+                                                               Description = "First Item",
+                                                               Name = "FIRST",
+                                                               Number = 1,
+                                                               Quantity = 2,
+                                                               UnitPrice = 1.99m,
+                                                               ItemUrl = "http://localhost/product?1"
+                                                           },
+                                                       new OrderDetailsItem
+                                                           {
+                                                               Description = "Second Item",
+                                                               Name = "2ND",
+                                                               Number = 2,
+                                                               Quantity = 1,
+                                                               UnitPrice = 11m, 
+                                                               ItemUrl = "http://localhost/product?2"
+                                                           },
+                                                   
+                                                       new OrderDetailsItem
+                                                           {
+                                                               Description = "Subscription",
+                                                               Name = "SBC",
+                                                               Number = 1,
+                                                               Quantity = 1,
+                                                               UnitPrice =0.0m, 
+                                                               ItemUrl = "http://localhost/subscription?1",
+                                                               IsRecurrentPayment = true
+                                                           }
+                                                   },
+                    ShippingTotal = 2m,
+                    OrderTotal = 16.98m 
+                };
+    }
+
+    public class When_starting_express_checkout_with_multiple_subsriptions : ExpressCheckoutContext
+    {
+        Behaves_like<SuccessfulExpressCheckoutBehavior> a_successful_express_checkout;
+        Establish context = () =>
+                OrderDetails = new OrderDetails
+                {
+                    OrderDescription = "Some order",
+                    Items = new[]
+                                                   {
+                                                       new OrderDetailsItem
+                                                           {
+                                                               Description = "Subscription",
+                                                               Name = "SBC",
+                                                               Number = 1,
+                                                               Quantity = 1,
+                                                               UnitPrice = 0.0m, 
+                                                               ItemUrl = "http://localhost/subscription?1",
+                                                               IsRecurrentPayment = true
+                                                           },
+                                                           new OrderDetailsItem
+                                                           {
+                                                               Description = "Subscription2",
+                                                               Name = "SBC2",
+                                                               Number = 1,
+                                                               Quantity = 1,
+                                                               UnitPrice = 10.0m, 
+                                                               ItemUrl = "http://localhost/subscription?2",
+                                                               IsRecurrentPayment = true
+                                                           }
+                                                   },
+                    ShippingTotal = 0m,
+                    OrderTotal = 10m
+                };
     }
 }
---
+++ Moolah/Moolah.Specs/PayPal/PayPalRecurringProfileSpecs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Machine.Fakes;
+using Machine.Specifications;
+using Moolah.PayPal;
+
+namespace Moolah.Specs.PayPal
+{
+    class PayPalRecurringProfileSpecs
+    {
+        [Subject(typeof(PayPalExpressCheckout))]
+        public class When_registering_a_recurring_profile : PayPalExpressCheckoutContext
+        {
+            It should_return_the_correct_result = () =>
+                Result.ShouldEqual(ExpectedResult);
+
+            Establish context = () =>
+            {
+                ExpectedResult = new PayPalRecurringProfileResponse();
+                RequestBuilder.WhenToldTo(x => x.CreateRecurringPaymentsProfile(Profile, Token))
+                    .Return(HttpUtility.ParseQueryString(Request));
+                ResponseParser.WhenToldTo(x => x.CreateRecurringProfile(Param<NameValueCollection>.Matches(r => r.ToString() == Response)))
+                    .Return(ExpectedResult);
+            };
+
+            private Because of = () =>
+                                 Result = SUT.CreateRecurringPaymentsProfile(Profile, Token);
+
+            static IPaymentResponse Result;
+            static PayPalRecurringProfileResponse ExpectedResult;
+            const string Token = "tokenValue";
+            static RecurringProfile Profile = new RecurringProfile();
+            const string PayerId = "payerId";
+        }
+    }
+}
