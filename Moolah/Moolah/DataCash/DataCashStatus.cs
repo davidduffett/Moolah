0afe4897d3eab97a664f8321723d9cda796ed1ad
David Duffett <duffett.david@gmail.com>
2014-01-29 11:14:51 +1100

    Allow immediate authorisation for status 188 (#36)

--- Moolah/Moolah/DataCash/DataCashStatus.cs
+++ Moolah/Moolah/DataCash/DataCashStatus.cs
@@ -10,7 +10,7 @@ namespace Moolah.DataCash
         public const int Success = 1;
         public const int NotAuthorised = 7;
         public const int RequiresThreeDSecureAuthentication = 150;
-        static readonly int[] CanAuthoriseStatuses = new[] { 158, 159, 160, 162, 163, 164, 165, 173, 170, 183, 186, 187, 189 };
+        static readonly int[] CanAuthoriseStatuses = new[] { 158, 159, 160, 162, 163, 164, 165, 173, 170, 183, 186, 187, 188, 189 };
 
         /// <summary>
         /// Whether this transaction can be immediately authorised without 3DS payer verification.
